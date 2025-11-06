da3e771a0c169a7b1a2bae022f9e197608396aeb
VirgilioDefeo <virgilio.defeo9@gmail.com>
2025-11-06 12:28:54 +0100

    rettifica

---
+++ types.ts
@@ -0,0 +1,14 @@
+// types.ts
+export type Game = {
+  title: string;          
+  category: string;      
+  studio?: string;       
+  platform?: string;     
+  releaseYear?: number;  
+  price?: number;        
+  rating?: number;       
+  players?: string;      
+  description?: string;  
+  image?: string;        
+  tags?: string[];      
+};
